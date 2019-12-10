98d517cd284c8d0d3e89bea0a5cfd83404ae9d0a
TBCII <messageteddy@gmail.com>
2019-12-10 23:53:20 +0800

    Changed hello world to welcome

--- src/gui/home/Main.java
+++ src/gui/home/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("fxml/FirstPage.fxml"));
 
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Welcome");
 
         primaryStage.setScene(new Scene(root, 1920, 950));
        primaryStage.setFullScreen(true);
