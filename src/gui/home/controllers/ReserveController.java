c9d528eb7613ba373b35cf44d2aa078780f0e832
Emil Lopez <emiljohn24@gmail.com>
2019-12-11 14:32:55 +0800

    Connected some GUI

--- src/gui/home/controllers/ReserveController.java
+++ src/gui/home/controllers/ReserveController.java
@@ -1,5 +1,21 @@
 package gui.home.controllers;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXDatePicker;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class ReserveController {
+    @FXML
+    private JFXDatePicker datePicker;
+    @FXML
+    private Label priceLabel;
+    @FXML
+    private JFXComboBox selectedCompany;
     //TODO: Start reserve controller
+
+    @FXML
+    private void initialize(){
+
+    }
 }
