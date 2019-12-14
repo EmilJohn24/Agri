20ac6d2730686db9df9d2002613f2f47ef1e0fda
Emil Lopez <emiljohn24@gmail.com>
2019-12-14 17:43:41 +0800

    Added transaction history

--- src/gui/home/controllers/MapController.java
+++ src/gui/home/controllers/MapController.java
@@ -1,59 +1,21 @@
 package gui.home.controllers;
 
-import gui.home.controllers.customNodes.MapNode;
+import com.jfoenix.controls.JFXListView;
 import javafx.fxml.FXML;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 import management.GlobalSessionHolder;
-import map.GlobalFarmMap;
-import map.Point;
-
-import java.util.HashMap;
-import java.util.LinkedList;
+import management.account_types.Consumer;
+import market.Item;
 
 public class MapController {
-    @FXML
-    private Pane mapContainer;
-    private Point userPoint;
-    private MapNode userNode;
-    private HashMap<Point, MapNode> nodes;
 
-    private void clickedMapNode(MapNode clickedMapNode){
-        if (userPoint == null){
-            GlobalSessionHolder.currentSession.getSessionAccount().setFarmLocation(clickedMapNode.getAssociatedPoint());
-        }
-        else{
-            LinkedList<Point> routeTo = GlobalFarmMap.farmMap.shortestPath(userPoint, clickedMapNode.getAssociatedPoint());
-            for (Point point : routeTo){
-                nodes.get(point).setFill(Color.RED);
-            }
-        }
-        loadPoints();
-    }
+    public JFXListView<String> transactionList;
 
     @FXML
     private void initialize(){
-        nodes = new HashMap<>();
-        loadPoints();
-    }
-
-
-    private void loadPoints(){
-        userPoint = GlobalSessionHolder.currentSession.getSessionAccount().getFarmLocation();
-        mapContainer.getChildren().removeAll(mapContainer.getChildren());
-        for (Point point : GlobalFarmMap.farmMap.getPoints()){
-            MapNode newNode = new MapNode(point);
-            newNode.setFill(Color.GREEN);
-            if (point == userPoint) userNode = newNode;
-            nodes.put(point, newNode);
-            newNode.setOnMouseClicked(event -> clickedMapNode(newNode));
-            newNode.setRadius(20.0);
-            mapContainer.getChildren().add(newNode);
+        Consumer consumerAdapterAccount = (Consumer) GlobalSessionHolder.currentSession.getSessionAccount();
+        for (Item transaction : consumerAdapterAccount.getItemsBought()){
+            transactionList.getItems().add(transaction.getSeller().getName() + " - " + transaction.getQuantity() + " kg -"
+                                        + transaction.getProduct().getName() + " - " + (transaction.getPrice() * transaction.getQuantity()));
         }
-        if (userNode != null){
-            userNode.setFill(Color.YELLOW);
-        }
-
-
     }
 }
